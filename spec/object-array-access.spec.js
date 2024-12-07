7a8217abcc1f22629c27ea839d6db12eaed4b4ec
Alex Aaron <alexfrancisaaron@gmail.com>
2024-12-07 14:00:17 -0600

    added README.md instructions for object and array access first prompt

--- spec/object-array-access.spec.js
+++ spec/object-array-access.spec.js
@@ -1,10 +1,13 @@
 describe('object & array access', function(){
+    
     describe('getFirstPrepObservation', function(){
         it('should return a string', function(){
             assert.equal(typeof getFirstPrepObservation(student), 'string');
         });
-        it('should return a correctly formmated string', function(){
-            assert.equal(getFirstPrepObservation(student), "Stephanie was absent from class due to her powering being out - 11/06/2024")
+        it('should return a correctly formatted string', function(){
+            assert.equal(getFirstPrepObservation(student), "Stephanie was absent from class due to her powering being out. - 11/06/2024")
         });
-    })
+    });
+
+    
 });
