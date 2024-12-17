dace92631cd074ced5cdf1124bcc94e8f1ee147c
Alex Aaron <alexfrancisaaron@gmail.com>
2024-12-17 01:50:10 +0000

    created first es6 features prompt

--- spec/es6-features.spec.js
+++ spec/es6-features.spec.js
@@ -1,5 +1,15 @@
 describe("ES6 Features", function(){
     describe("getInfoString", function(){
+        it('should be written as an arrow function', function(){
+            const func = getInfoString.toString();
+            const regex = /\([a-zA-Z]+\)\s=>\s{/g;
+            const match = func.match(regex);
+            if (match){
+                assert.equal(match.length > 0, true);
+            } else {
+                expect(match).to.not.equal(null);
+            }
+        });
         it('should return a string', function(){
             assert.equal(typeof getInfoString(films[1]), 'string');
         });
@@ -37,6 +47,37 @@ describe("ES6 Features", function(){
                 matches = matches.concat(match1);
             } else if (match2){
                 console.log('hit this condition');
+                matches = matches.concat(match2);
+            }
+            assert.equal(matches.length > 0, true);
+        });
+    });
+    describe("getNewGenreTagsArray", function(){
+        const func = getNewGenreTagsArray.toString();
+        it('should be written as an arrow function', function(){
+            const regex = /\([a-zA-Z]+\)\s=>\s{/g;
+            const match = func.match(regex);
+            if (match){
+                assert.equal(match.length > 0, true);
+            }  else {
+                expect(match).to.equal(null);
+            }
+        });
+        it('should return an array', function(){
+            assert.equal(Array.isArray(getNewGenreTagsArray(films[1])), true);
+        });
+        it('should return a correct array', function(){
+            assert.equal(getNewGenreTagsArray(films[1], "Horror"), ["Suspense", "Drama", "Horror"]);
+        });
+        it('should use the spread operator', function(){
+            const regex1 = /\[(\s...[a-zA-Z]+|...[a-zA-Z]+)(\s\]|\])/g;
+            const regex2 = /\[(\s...[a-zA-Z]+|...[a-zA-Z]+)(,\s\]|,\])/g;
+            const match1 = func.match(regex1);
+            const match2 = func.match(regex2);
+            let matches = [];
+            if (match1){
+                matches = matches.concat(match1);
+            } else if (match2){
                 matches = matches.concat(match2);
             }
             assert.equal(matches.length > 0, true);
--- week-1/day-3/es6-features/es6-features.js
+++ week-1/day-3/es6-features/es6-features.js
@@ -1,3 +1,18 @@
+
+// Problem #1 // use destructuring on a single object
+let getInfoString;
+
+// Problem #2 // using spread operator on a simple array
+let getNewGenreTagsArray = (film, newTag) => {
+    const [ genreTags ] = film.genreTags;
+    const output = [ ...genreTags, ];
+    output.push(newTag);
+    return output;
+}
+
+
+// destructuring on each object using a for loop
+
 // using spread operator on a simple arrays
 
 // using spread operator on objects or objects in arrays
@@ -5,11 +20,3 @@
 // every function should be an arrow function
 
 // using 
-
-// Problem #1 // use destructuring on a single object
-let getInfoString;
-
-
-
-
-// destructuring on each object using a for loop
