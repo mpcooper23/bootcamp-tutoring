675837fe7670e6d99b47c820e1d70609cf8ead86
mpcooper23 <mpcooper23@gmail.com>
2024-12-18 15:04:44 -0600

    1

--- week-1/day-2/loops/loops.js
+++ week-1/day-2/loops/loops.js
@@ -1,7 +1,27 @@
 
 // Problem #1 //
+/**
+ * 
+ * 1. Create a function called `logLocationAndTime` that takes in one parameter - `array` - 
+ * which represents an array of student objects like the one featured in data.js. 
+ * This function should use a for loop to iterate backwards through the input array, 
+ * and each iteration it should log the current student's location followed by a dash and their timezone.
+```javascript
+// example output
+logLocationAndTime(students);
+/*
+LOGS =>
+Baltimore, MD - EST
+Atlanta, GA - EST
+New Orleans, LA - CST
+New Orleans, LA - CST
+*/
+
+
 function logLocationAndTime(array){
-   
+   for (let i = array.length - 1; i >= 0; i--){
+    console.log(array[i].location + ' - ' + array[i].timeZone)
+   }
 }
 
 // Problem #2 //
