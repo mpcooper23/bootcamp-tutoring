81b07da017575238009ec61944bda7df88e8c6d8
mpcooper23 <mpcooper23@gmail.com>
2024-12-19 14:46:54 -0600

    i

--- week-1/day-3/es6-features/es6-features.js
+++ week-1/day-3/es6-features/es6-features.js
@@ -9,36 +9,94 @@
 ```javascript
 // example output
 getInfoString(films[1]); // => "Blue Velvet (1986) - diredted by David Lynch"
+
+I:
+O:
+C:
+E:
+
  */
 
 let getInfoString;
 
 // Problem #2 // using spread operator on a simple array
 /**
- * 2. Create a function called `getNewGenreTagsArray` that takes in two parameters - `film` & `newTag`; `film` represents a film object like the ones in the `films` array in the data.js file. This function should use destructuring to create a variable for the input `film` object's genreTags array (meaning instead of accessing that property directly, you should create a variable assigned to that array). This function should then use the spread operator to create a new array with all of the values from that variable copied in. Once the copy is made, the function should add the `newTag` input into the copied array and return that array at the end of the function.
+ * 2. Create a function called `getNewGenreTagsArray` that takes in two parameters 
+ * - `film` & `newTag`; `film` represents a film object like the ones in the `films`
+ * array in the data.js file. This function should use destructuring to create a variable 
+ * for the input `film` object's genreTags array (meaning instead of accessing that 
+ * property directly, you should create a variable assigned to that array). 
+ * This function should then use the spread operator to create a new array with 
+ * all of the values from that variable copied in. Once the copy is made, the function 
+ * should add the `newTag` input into the copied array and return that array at the end of 
+ * the function.
 ```javascript
+
+I:
+O:
+C:
+E:
+
  */
 
 let getNewGenreTagsArray;
 
 // Problem #3 // using spread operator to create a copy of an object
 /**
- * 3. Create a function called `getNewObject` that takes in three parameter - `film`, `key`, `value`; `film` represents a film object like the ones in the `films` array in the data.js file; `key` represents a new key to be added; `value` represents the value that should be attached to the key. This function should use the spread operator to make a copy of the input object. After the copy is made, the function should add the new key and the value attached to that key to the copy and then return it.
- */
+ * 3. Create a function called `getNewObject` that takes in three parameter 
+ * - `film`, `key`, `value`; `film` represents a film object like the ones in 
+ * the `films` array in the data.js file; `key` represents a new key to be added; 
+ * `value` represents the value that should be attached to the key. This function 
+ * should use the spread operator to make a copy of the input object. After the 
+ * copy is made, the function should add the new key and the value attached to 
+ * that key to the copy and then return it.
+
+I:
+O:
+C:
+E:
+*/
 
 let getNewObject;
 
 // Problem #4 // destructuring on each object using a for loop
 /**
- * 4. Create a function called `logAwards` that takes in two parameters - `films` & `director`; `films` represents an array of films like the one featured in data.js; `director` represents the name of a director we are searching for. This function should iterate through the input array and find the director. Once the director is found, the function should iterate through the director's `academyAwards` array. At each iteration, the function should use destructuring to create variables for each object's `filmTitle`, `category`, and `status` properties. The function should use template literal syntax to log each award's status followed by a colon and the title of the film and category it was nominated in.
+ * 4. Create a function called `logAwards` that takes in two parameters 
+ * - `films` & `director`; `films` represents an array of films like the 
+ * one featured in data.js; `director` represents the name of a director 
+ * we are searching for. This function should iterate through the input 
+ * array and find the director. Once the director is found, the function 
+ * should iterate through the director's `academyAwards` array. At 
+ * each iteration, the function should use destructuring to create variables 
+ * for each object's `filmTitle`, `category`, and `status` properties. The 
+ * function should use template literal syntax to log each award's status 
+ * followed by a colon and the title of the film and category it was nominated in.
 ```javascript
- */
+
+I:
+O:
+C:
+E:
+*/
 
 let printAwards;
 
 // Problem #5 // use destructing on an array of arrays
 /**
- * 5. Create a function called `updateAward` that takes in 4 parameters - `film`, `nominatedFilm`, `category`, `updates`; `film` represents a film object like the ones in the `films` array in the data.js file; `nominatedFilm` represents a string of a film the director of the input film was nominated for; `category` represents the specific category the `nominatedFilm` was nominated in. `status` represents an array of arrays, where each subarray contains a key on the nomination that we want to alter and a value we want to attach at the key. This function should find the `nominatedFilm` in the input film's `academyAwards` array. Once found, this function should use the spread operator to create a copy of the nomination. The function should then iterate through the  `updates` array to update each key and value on the nomination. Make sure you use destructuring on each subarray to create variables for the key and value. This function should return the updated copy.
+ * 5. Create a function called `updateAward` that takes in 4 parameters 
+ * - `film`, `nominatedFilm`, `category`, `updates`; `film` represents 
+ * a film object like the ones in the `films` array in the data.js file; 
+ * `nominatedFilm` represents a string of a film the director of the input 
+ * film was nominated for; `category` represents the specific category the 
+ * `nominatedFilm` was nominated in. `status` represents an array of arrays, 
+ * where each subarray contains a key on the nomination that we want to alter 
+ * and a value we want to attach at the key. This function should find the 
+ * `nominatedFilm` in the input film's `academyAwards` array. Once found, 
+ * this function should use the spread operator to create a copy of the 
+ * nomination. The function should then iterate through the  `updates` 
+ * array to update each key and value on the nomination. Make sure you 
+ * use destructuring on each subarray to create variables for the key 
+ * and value. This function should return the updated copy.
 ```javascript
 // example output
 updateAward(films[1], 'Blue Velvet', 'Best Director', [ ['filmTitle', 'BLUE VELVET'], ['status', 'NOMINATION'] ]);
@@ -50,7 +108,12 @@ RETURNS =>
                 category: 'Best Director',
                 status: 'NOMINATION'
             };
-*/
+
+I:
+O:
+C:
+E:
+            */
 
 
 let updateAward;
