d08ff3b68fc88f1b98dffe695d8260123761326a
mpcooper23 <mpcooper23@gmail.com>
2025-03-06 16:42:46 -0600

    1

--- week-1/day-1/object-array-access/object-array-access.js
+++ week-1/day-1/object-array-access/object-array-access.js
@@ -1,29 +1,33 @@
 
+//function getFirstObservation(object){
+   // return object.courses.filter(course => {course.length - 1})
+  //  }
+
+
+
 // Problem #1 //
 
 /**
- * I: Inputs an object
- * O: returns first prep observation text as string, followed by the date it was made
- * C:
- * E: N/A
- *
- 
 
-function getFirstPrepObservation(object){
-   return object.courses[0].observations[0].text + " - " + object.courses[0].observations[0].dateObserved
-}/*
 1. Create a function called `getFirstPrepObservation` that takes in one parameter - `object` - which 
 represents a student object like the one featured in the data.js file. This function should use the 
 principles of object and array access to return a string of the text of the student's first observation 
 from the Prep phase (assume all students will have a Prep observation) followed by the date the observation 
-was made.*/
+was made.
+I: object
+O: string of teh student's first obs from Prep followed by date obs was made
 
-function getFirstObservation(object){
-return object.courses.filter(course => {course.length - 1})
+*/
+
+
+function getFirstPrepObservation(object){
+ if (student.courses[0].courseName = 'Prep'){
+    let date = student.courses[0].observations[0].dateObserved;
+    let text = student.courses[0].observations[0].text
+    console.log(text)
+    return `${text} - ${date}`
+ }
 }
-
-
-
 
 
 
@@ -38,17 +42,9 @@ return object.courses.filter(course => {course.length - 1})
  * name and role of the author of the student's last observation from the last phase they have 
  * attempted (assume the last object in the courses array is the last one chronologically). 
  * You SHOULD NOT HARDCODE
- * I: object
- * O: a string of the name and role of the author of the
- * student's last observation from the last phase they attempted
- * C: Don't hardcode last array element in courses (i.e. use object.courses.length - 1)
- * E:
- *
  */
 // Problem #2 //
-function getLastObservationAuthor(object){
-   return object.courses[1].observations[1].author.name + " - " + object.courses[1].observations[1].author.role
-}
+
 /**
  * Create a function called `getLastCourseNameAndStatus` that takes in one parameter - `object`, 
  * which represents a student object like the one featured in data.js. This function should use 
@@ -61,8 +57,8 @@ function getLastObservationAuthor(object){
  *
  */
 // Problem #3 //
-function getLastCourseNameAndStatus(object){
-    return object.courses[1].courseName + " - " + object.courses[1].status }
+
+
 /**
  * 
  *
@@ -88,14 +84,6 @@ number of courses.
 // Problem #4 //
 
 
-function createSimplifiedObject(object){
-    return {
-name: object.firstName + ' ' + object.lastName,
-age: object.age,
-location: object.city + ', ' + object.state,
-courses: object.courses.length
-    }
-}
 
 /**
  * 
@@ -118,29 +106,8 @@ Date of Last Course: 09/15/2023 (Bootcamp)
 
 // Problem #5 //
 
-function createString(object){
-    return object.firstName + ' ' + object.lastName[0] + '.' + '\n' + 'State & Time Zone:' + ' ' + object.state + ' - ' + object.timeZone + '\n' + 'Date of Last Course:' + ' ' + object.courses[1].dateOfLastAttempt + ' ' + '(' + object.courses[1].courseName + ')' + '\n'
-}
 
 
 
 
-
-
-
-
-// function getLastRoleAndDateArray(array){
-//     const output = [];
-//     for (let i = 0; i < array.length; i++){
-//         const course = array[i].courses[array[i].courses.length - 1];
-//         let object;
-//         if (course.observations.length > 1){
-//             object = course.observations[course.observations.length - 1];
-//         } else {
-//             object = course.observations[0];
-//         }
-//         output.push(object.author.role + " - " + object.dateObserved);
-//     }
-//     return output;
-// }
 
